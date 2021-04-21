ad566f4aab374ef5a3c0b9214d063428b78c4952
Kenneth Lu Diaz <kenneth_ludiaz@homework.cs.uri.edu>
2021-04-21 17:03:09 -0400

    Fixes compile bug

--- um.c
+++ um.c
@@ -1,5 +1,18 @@
 #include "um.h"
 
+
+int main(int argc, char** argv) {
+    assert(argc < 3);
+    FILE * fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+            fp = stdin;
+    }
+    assert(fp);
+    run(fp);
+    fclose(fp);
+}
+
+
 // Program will never be null
 int run(FILE * program) {
     uint32_t  r[8] = { 0 };
